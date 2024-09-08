26fc2c8832f5414436f076fa2e879d4ca95b4a4e
Weixuan Ning <73421965+WeixuanPlant@users.noreply.github.com>
2024-09-08 14:17:44 -0500

    Update 03_GeneTrees_ASTRAL_GCF.sh

--- 01_HybPiperRunning/03_GeneTrees_ASTRAL_GCF.sh
+++ 01_HybPiperRunning/03_GeneTrees_ASTRAL_GCF.sh
@@ -1,29 +1,35 @@
-#!/bin/bash -e
-#SBATCH --job-name=angiosperms353_aln  #job name (shows up in the queue)
-#SBATCH --account=massey02696
-#SBATCH --cpus-per-task=10   # number of CPUs per task (1 by default)
-#SBATCH --time=10:00:00 #Walltime (HH:MM:SS)
-#SBATCH --mem=10G  #Memory in GB 
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=50G 
+#SBATCH --time=0-05:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --open-mode=append
+#SBATCH --output="job.vcf_n319.%J.out"
+#SBATCH --job-name="jointGeno_n319"
+#SBATCH --array=1-351%2
 
-module load IQ-TREE/2.2.0.5-gimpi-2022a
-iqtree2 -S aln/ --prefix ./loci_gt07_n15_Azorella -T AUTO -B 1000 --undo
+module load iqtree2/2.2.2.7-py310-kcqit6n
+file=$(ls -1 ../aln/*_trim.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
+name=$(basename $file _trim.fasta)
+iqtree2 -s ../aln/${name}_trim.fasta --prefix $name -T AUTO -B 1000 -m MFP
 
 
-# collapes the node bootstrapping value less than 30%
-./nw_ed ./loci_gt07_n20_Azorella.treefile  'i & b<=30' o > loci_gt07_n20_Azorella.bs30.treefile
+# collapes the node bootstrapping value less than 10%
+./nw_ed ./loci_gt07_n20_Azorella.treefile  'i & b<=10' o > loci_gt07_n20_Azorella.bs10.treefile
 
-java -jar /scale_wlg_nobackup/filesets/nobackup/massey02696/Azorella/Bioinformatics/Astral/astral.5.7.7.jar -i loci_gt07_n20_Azorella.bs30.treefile -o loci_gt07_n20_Azorella.bs30.astral.treefile
+java -jar /scale_wlg_nobackup/filesets/nobackup/massey02696/Azorella/Bioinformatics/Astral/astral.5.7.7.jar -i loci_gt07_n20_Azorella.bs10.treefile -o loci_gt07_n20_Azorella.bs10.astral.treefile
 
 # calculating the condordance factors
 module load ETE/3.1.1-gimkl-2020a-Python-3.8.2
 
 #make an outgroup.txt for rerooting trees
-python3 reroot_trees.py loci_gt07_n20_Azorella.bs30.treefile outgroup.txt > loci_gt07_n20_Azorella.bs30.rerooted.treefile
-python3 reroot_trees.py loci_gt07_n20_Azorella.bs30.astral.treefile outgroup.txt > loci_gt07_n20_Azorella.bs30.astral.rerooted.treefile
-java -jar  -Xmx16g phyparts-0.0.1-SNAPSHOT-jar-with-dependencies.jar -a 1 -v -d loci_gt07_n20_Azorella.bs30.rerooted.treefile -m loci_gt07_n20_Azorella.bs30.astral.rerooted.treefile  -o out_n20
+python3 reroot_trees.py loci_gt07_n20_Azorella.bs10.treefile outgroup.txt > loci_gt07_n20_Azorella.bs10.rerooted.treefile
+python3 reroot_trees.py loci_gt07_n20_Azorella.bs10.astral.treefile outgroup.txt > loci_gt07_n20_Azorella.bs10.astral.rerooted.treefile
+java -jar  -Xmx16g phyparts-0.0.1-SNAPSHOT-jar-with-dependencies.jar -a 1 -v -d loci_gt07_n20_Azorella.bs10.rerooted.treefile -m loci_gt07_n20_Azorella.bs10.astral.rerooted.treefile  -o out_n20
  
-grep 'Azly' loci_gt07_n4_Azorella.bs30.treefile  > loci_gt07_n4_Azorella.bs30.outgroup.treefile
-python3 ../phypartspiecharts/reroot_trees.py loci_gt07_n4_Azorella.bs30.outgroup.treefile utgroup.list  > loci_gt07_n4_Azorella.bs30.outgroup.rerooted.treefile
+grep 'Azly' loci_gt07_n4_Azorella.bs10.treefile  > loci_gt07_n4_Azorella.bs10.outgroup.treefile
+python3 ../phypartspiecharts/reroot_trees.py loci_gt07_n4_Azorella.bs10.outgroup.treefile utgroup.list  > loci_gt07_n4_Azorella.bs10.outgroup.rerooted.treefile
 
 
 
